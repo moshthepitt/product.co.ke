9a0dfd34bad183ded481a3888129fc0f2847058a
moshthepitt <kelvin@jayanoris.com>
2016-05-06 19:01:12 +0300

    add way to add new posts

--- links/urls.py
+++ links/urls.py
@@ -6,7 +6,7 @@ from .views import LinkUpdate, LinkAdd, LinkView, UserLinksView, UserLinkDelete
 urlpatterns = [
     url(r'^add/$', login_required(LinkAdd.as_view()), name='link_add'),
     url(r'^edit/(?P<pk>\d+)/$', login_required(LinkUpdate.as_view()), name='link_edit'),
-    url(r'^my-submissions/$', UserLinksView.as_view(), name='user_links'),
-    url(r'^delete/(?P<pk>\d+)/$', UserLinkDelete.as_view(), name='user_delete_link'),
+    url(r'^my-submissions/$', login_required(UserLinksView.as_view()), name='user_links'),
+    url(r'^delete/(?P<pk>\d+)/$', login_required(UserLinkDelete.as_view()), name='user_delete_link'),
     url(r'^(?P<pk>\d+)/$', LinkView.as_view(), name='link_detail'),
 ]
