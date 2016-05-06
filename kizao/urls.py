a1a6ce8509ec002c885d39d8329de83e0f40d2d7
moshthepitt <kelvin@jayanoris.com>
2016-05-06 16:44:11 +0300

    implement ajax django messages

--- kizao/urls.py
+++ kizao/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.static import serve
-
+from django.views.generic.base import TemplateView
 from django.conf import settings
 
 from core.views import HomePageView
@@ -11,6 +11,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^links/', include('links.urls', namespace='links')),
+    url(r'^django-messages', TemplateView.as_view(template_name="django_messages.html"),
+        name='django_messages'),
     url(r'^page/', include('django.contrib.flatpages.urls')),
 ]
 
