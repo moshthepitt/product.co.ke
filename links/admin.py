537feb8f79e40fb45fccaf2faacf0a2c3a6dba2a
moshthepitt <kelvin@jayanoris.com>
2016-05-12 15:04:12 +0300

    add admin for Link

--- links/admin.py
+++ links/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Link
+
+
+class LinkAdmin(admin.ModelAdmin):
+    search_fields = ['title__icontains', 'description']
+    list_display = ['title', 'user', 'domain', 'active']
+    list_filter = ['user', 'active']
+    date_hierarchy = 'created_on'
+
+
+admin.site.register(Link, LinkAdmin)
